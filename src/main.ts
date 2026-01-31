1943cab0b8008335954ed7651b22f194b6be41a5
stepan.rutz <stepan.rutz@gmx.de>
2026-01-31 12:02:27 +0100

    explicit border colors

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,4 @@ createApp(App)
     .mount('#app')
 
 console.log("%cVuesolitaire", "font-size:36px; color: #9c1020; font-Family: 'Sans'; font-weight: 700")
-console.log("%cwritten by Stepan Rutz. Source at https://github.com/srutz/vuesolitaire/", "font-size:18px; color: #9c1020; font-Family: 'Noto Sans'; font-weight: 700")
+console.log("%cwritten by Stepan Rutz. Tailwind4 edition. Source at https://github.com/srutz/vuesolitaire/", "font-size:18px; color: #9c1020; font-Family: 'Noto Sans'; font-weight: 700")
