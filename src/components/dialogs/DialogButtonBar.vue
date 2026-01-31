1fe8683e2d3263e4664cd3091730ab3040b420d1
stepan.rutz <stepan.rutz@gmx.de>
2026-01-31 12:01:29 +0100

    explicit border colors

--- src/components/dialogs/DialogButtonBar.vue
+++ src/components/dialogs/DialogButtonBar.vue
@@ -1,4 +1,4 @@
 <template>
-    <div class="flex justify-end p-4 border-t gap-4"><slot></slot></div>
+    <div class="flex justify-end p-4 border-t border-gray-300 gap-4"><slot></slot></div>
 </template>
 
--- src/components/dialogs/ModalTitle.vue
+++ src/components/dialogs/ModalTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-between items-center p-4 border-b">
+    <div class="flex justify-between items-center p-4 border-b border-gray-300">
         <h2 class="text-xl text-black font-semibold">{{ title }}</h2>
         <button @click="onClose" class="text-gray-500 hover:text-gray-800">
             &times;
